00d2d8bfaeadbf23013e814e93ab16b34a870cb8
lsowen <logan@s1network.com>
2015-01-22 07:09:49 -0500

    whitespace cleanup

--- client.py
+++ client.py
@@ -3,8 +3,6 @@ from thriftpy.thrift import TClient
 from .rclient import RClient
 from .transport import TTransport_R, TBinaryProtocol_R
 
-
-
 def get_client(service, uri, exchange, queue):
     client = RClient(uri)
     c_transport = TTransport_R(client, TTransport_R.CLIENT, amqp_exchange = exchange, amqp_queue = queue)
--- server.py
+++ server.py
@@ -1,15 +1,33 @@
 from .transport import TTransport_R, TBinaryProtocolFactory_R
 from .rclient import RClient
 
+
 from thriftpy.thrift import TClient, TProcessor
 from thriftpy.server import TThreadedServer, TSimpleServer
 from thriftpy.transport.transport import TBufferedTransportFactory
 
+class TThreadedServer_R(TThreadedServer):
+    def __init__(self, *args, **kwargs):
+        TThreadedServer.__init__(self, *args, **kwargs)
+        self._close_actions = []
+
+        def add_close_action(self, action):
+            self._close_actions.append(action)
+
+        def close(self):
+            self.closed = True
+            for action in self._close_actions:
+                print("shutdown action calling")
+                action()
+                print("shutdown action called")
+
+
 
 def get_server(service, responder, uri, exchange, queue):
-        rclient = RClient(uri)
-        s_transport = TTransport_R(rclient, TTransport_R.SERVER, amqp_exchange = exchange, amqp_queue = queue)
-        processor = TProcessor(service, responder)
-        server = TThreadedServer(processor, s_transport, TBufferedTransportFactory(), TBinaryProtocolFactory_R())
+    rclient = RClient(uri)
+    s_transport = TTransport_R(rclient, TTransport_R.SERVER, amqp_exchange = exchange, amqp_queue = queue)
+    processor = TProcessor(service, responder)
+    server = TThreadedServer_R(processor, s_transport, TBufferedTransportFactory(), TBinaryProtocolFactory_R())
 
-        return server
+    server.add_close_action(s_transport.shutdown)
+    return server
