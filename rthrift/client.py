ab75187ceeb29ec2e4879a05c9c1e94b47faeede
lsowen <logan@s1network.com>
2015-01-22 19:54:13 -0500

    make read timeout for client configurable

--- rthrift/client.py
+++ rthrift/client.py
@@ -3,12 +3,13 @@ from thriftpy.thrift import TClient
 from .rclient import RClient
 from .transport import TTransport_R, TBinaryProtocol_R
 
-def get_client(service, uri, exchange, queue):
+def get_client(service, uri, exchange, queue, read_timeout=None):
     client = RClient(uri)
     c_transport = TTransport_R(client, TTransport_R.CLIENT, amqp_exchange = exchange, amqp_queue = queue)
     c_proto = TBinaryProtocol_R(c_transport)
     client = TClient(service, c_proto)
-    c_transport.set_read_timeout(1)
+    if read_timeout is not None:
+        c_transport.set_read_timeout(read_timeout)
     c_transport.open()
 
     return client
